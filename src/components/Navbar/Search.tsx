19ccf118c632dd472664f657f65295307e38e5d6
Rishav Jindal <rishavj0212@gmail.com>
2024-01-04 01:14:31 +0530

    Query Dropdown

--- src/components/Navbar/Search.tsx
+++ src/components/Navbar/Search.tsx
@@ -1,41 +1,63 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import SearchIcon from "@mui/icons-material/Search";
-import { IconButton, Tooltip } from "@mui/material";
+import { IconButton, TextField } from "@mui/material";
 import { useRouter } from "next/navigation";
+import { Autocomplete } from "@mui/lab";
+import items from "@/product_names/names";
+
 
 function Search() {
   const [text, setText] = useState("");
-  const router = useRouter();
+  const router = useRouter();  
+  const [isDropdownOpen, setIsDropdownOpen] = useState(true);
+  
+  const inputRef = useRef(null);
+
+
 
   const addQuery = () => {
+    console.log(text)
+
     router.push("/products/" + text);
+    if (inputRef.current) {
+      inputRef.current.blur();
+    }
+    setText('')
+
+
+
   };
 
-  ///// with useEffect page is rendred everytime when we add a leeter in search
-
-  useEffect(() => {
-    if (text.length >= 1) {
-      addQuery();
+  const handleSelection = (e, value)=>{
+    // setText(value)    
+    router.push("/products/" + value);
+    if (inputRef.current) {
+      inputRef.current.blur();
     }
-  }, [text, addQuery]);
+    setText('')
 
-  ////
+  }
 
   return (
     <>
-      {/* <Tooltip title="Search Product" placement='top'>
-                <IconButton>
-                    <SearchIcon />
-                </IconButton>
-            </Tooltip> */}
-      <input
-        type="text"
-        placeholder="Search Product . . . "
-        value={text}
-        onChange={(e) => setText(e.target.value)}
+      
+      <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={items.map((item)=>(item.name))}
+          sx={{ width: 200}}
+          
+          renderInput={(params) => <TextField {...params} label="Search Product..."
+          onKeyPress={(e)=>{if (e.key==="Enter") addQuery()}}
+          inputRef={inputRef}
+        />}
+        // value={text}
+        onInputChange={(e, value)=>setText(value)}
+        value = {text}
+        onChange={handleSelection}
+                
       />
-      <button onClick={() => addQuery()}>Search</button>
     </>
   );
 }
