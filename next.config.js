d0beeef4665a3b9349ca4c885d8c918ce9417cb3
Rishav Jindal <rishavj0212@gmail.com>
2024-01-26 13:45:10 +0530

    added site-map

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // output: "export",
   
-  trailingSlash: true,
+  // trailingSlash: true,
 };
 
 module.exports = nextConfig;
