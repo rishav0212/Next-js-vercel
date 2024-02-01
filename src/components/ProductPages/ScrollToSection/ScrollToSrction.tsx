824e778eb3dd2b5778b4c8c5841053fddd76cafc
Rishav Jindal <rishavj0212@gmail.com>
2024-02-01 19:50:20 +0530

    corrected sectionbar

--- src/components/ProductPages/ScrollToSection/ScrollToSrction.tsx
+++ src/components/ProductPages/ScrollToSection/ScrollToSrction.tsx
@@ -6,33 +6,35 @@ import { useEffect, useRef, useState } from "react";
 const StyledButton = styled(Button)({
   marginInline: "5px",
   color: "rgb(230,230,230,1)",
-  width: "11.5em",
   backgroundColor: "rgb(150,150,165)",
+  minWidth: "auto",
+  width: "auto",
   border: "none",
+  // textTransform: "lowercase", //to lowercase the text
+  whiteSpace: "nowrap",
   zIndex: 12,
-  fontSize: "1.1em",
+  fontSize: "1.3em",
   padding: "10px",
   borderRadius: "10px",
   cursor: "pointer",
   boxShadow: "0 2px 4px rgb(0,0,0,0.4)",
-  transition: "boxShadow 0.3s ease, color 0.3s ease, transform 0.3s ease",
+  transition:
+    "boxShadow 0.3s ease-out, color 0.3s ease-out, transform 0.3s ease-out",
   "&:hover": {
     boxShadow: "0 4px 4px rgb(0,0,0,0.7)",
     color: "#fff",
     backgroundColor: "rgb(80,80,140)",
-    transform: "scale(1.05)",
+    transform: "scale(1.08)",
   },
 });
 const StyledNaviButton = styled("button")({
-  marginInline: "5px",
   color: "rgb(230,230,230,1)",
   backgroundColor: "rgb(150,150,165)",
+  border: "none",
   width: "2.3em",
   height: "2.3em",
-  border: "none",
-  zIndex: 12,
+  zIndex: 15,
   fontSize: "1.5em",
-  padding: "3px",
   borderRadius: "50%",
   cursor: "pointer",
   boxShadow: "0 2px 4px rgb(0,0,0,0.4)",
@@ -46,9 +48,10 @@ const StyledNaviButton = styled("button")({
 });
 
 const ScrollToSection = ({ Sections }) => {
-  const [startIndex, setStartIndex] = useState(0);
-  const [visibleButtons, setVisibleButtons] = useState([]);
   const containerRef = useRef(null);
+  const [isEndOfScroll, setIsEndOfScroll] = useState(false);
+  const [scrollLeft, setScrollLeft] = useState(0);
+
   const handleClick = (sectionId) => {
     const element = document.getElementById(sectionId);
 
@@ -57,9 +60,6 @@ const ScrollToSection = ({ Sections }) => {
       const scrollTop = document.documentElement.scrollTop;
       const barheight = containerRef.current.clientHeight;
       const targetOffset = rect.top + scrollTop - barheight - 67;
-      console.log(rect.top);
-      console.log(scrollTop);
-      console.log(barheight);
 
       window.scrollTo({
         top: targetOffset,
@@ -67,68 +67,86 @@ const ScrollToSection = ({ Sections }) => {
       });
     }
   };
+  const handleScroll = (direction) => {
+    const container = containerRef.current;
+    if (container) {
+      const scrollAmount = direction === "left" ? -200 : 200;
+      let left = scrollLeft + scrollAmount > 0 ? scrollLeft + scrollAmount : 0;
+      left =
+        containerRef.current.scrollWidth - containerRef.current.clientWidth <
+        scrollLeft + scrollAmount
+          ? containerRef.current.scrollWidth - containerRef.current.clientWidth
+          : scrollLeft + scrollAmount;
+      container.scrollTo({
+        left: left,
+        behavior: "smooth",
+      });
+      setScrollLeft(left);
+    }
+  };
 
   useEffect(() => {
-    const updateVisibleButtons = () => {
-      if (containerRef.current) {
-        const containerWidth = containerRef.current.clientWidth;
-        const buttonWidth =
-          (window.innerHeight * 0.007 + window.innerWidth * 0.003) * 12;
-        const nextPrevWidth =
-          (document.getElementById("prev")
-            ? document.getElementById("prev").clientWidth
-            : 0) +
-          (document.getElementById("next")
-            ? document.getElementById("next").clientWidth
-            : 0);
+    setIsEndOfScroll(
+      containerRef.current.scrollWidth - containerRef.current.clientWidth <
+        scrollLeft
+    );
+  }, [scrollLeft]);
 
-        const maxButtons = Math.floor(
-          (containerWidth - nextPrevWidth-10) / buttonWidth
-        );
+  useEffect(() => {
+    const container = containerRef.current;
 
-        setVisibleButtons(
-          Sections.slice(startIndex, startIndex + maxButtons - 1)
-        );
-      }
+    const handleScroll = () => {
+      setScrollLeft(container.scrollLeft);
     };
 
-    updateVisibleButtons();
-    window.addEventListener("resize", updateVisibleButtons);
+    container.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener("resize", updateVisibleButtons);
+      container.removeEventListener("scroll", handleScroll);
     };
-  }, [Sections, startIndex]);
-
-  const handleNext = () => {
-    setStartIndex((prevIndex) => Math.min(prevIndex + 2, Sections.length - 1));
-  };
-
-  const handlePrev = () => {
-    setStartIndex((prevIndex) => Math.max(prevIndex - 2, 0));
-  };
+  }, [containerRef]);
 
   return (
     <Grid
-      container
       ref={containerRef}
       position={"sticky"}
       zIndex={10}
       alignItems={"center"}
-      justifyContent={"center"}
       marginTop={1}
       paddingY={1}
       sx={{
+        overflowY: "hidden",
+        whiteSpace: "nowrap",
         top: { md: "9.3vh", sm: "9.3vh", xs: "6.3vh" },
         backgroundColor: "#f0f0f0",
+        WebkitOverflowScrolling: "touch",
+        "&::-webkit-scrollbar": {
+          display: "none",
+        },
       }}
     >
-      {startIndex !== 0 ? (
-        <StyledNaviButton id="prev" onClick={handlePrev}>
-          {"<"}
-        </StyledNaviButton>
+      {scrollLeft > 0 ? (
+        <div
+          style={{
+            minWidth: "8em",
+            height: "6em",
+            display: "inline-flex",
+            padding: 5,
+            background:
+              "linear-gradient(to left, rgba(240,240,240,0.0) 0%, rgba(240,240,240,1) 35%)",
+            position: "sticky",
+            left: "0",
+            zIndex: 13,
+            alignContent: "center",
+          }}
+        >
+          <StyledNaviButton onClick={() => handleScroll("left")}>
+            {"<"}
+          </StyledNaviButton>
+        </div>
       ) : null}
-      {visibleButtons.map((item, index) => (
+
+      {Sections.map((item, index) => (
         <StyledButton
           key={item}
           id={index.toString()}
@@ -137,10 +155,26 @@ const ScrollToSection = ({ Sections }) => {
           {item}
         </StyledButton>
       ))}
-      {startIndex + visibleButtons.length < Sections.length ? (
-        <StyledNaviButton id="next" onClick={handleNext}>
-          {">"}
-        </StyledNaviButton>
+      {!isEndOfScroll ? (
+        <div
+          style={{
+            minWidth: "8em",
+            height: "6em",
+            padding: 5,
+            display: "inline-flex",
+            background:
+              "linear-gradient(to right, rgba(240,240,240,0.0) 0%, rgba(240,240,240,1) 50%)",
+            position: "sticky",
+            right: "0",
+            zIndex: 13,
+            justifyContent: "right",
+            alignContent: "center",
+          }}
+        >
+          <StyledNaviButton onClick={() => handleScroll("right")}>
+            {">"}
+          </StyledNaviButton>
+        </div>
       ) : null}
     </Grid>
   );
